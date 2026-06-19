65ba2751ec861c29a2889ace78ff3cb0829273a1
Tal Kohavy <tal.kohavy@controlup.com>
2026-06-19 12:58:00 +0300

    HealthCheckModule is attached regardless

--- src/modules/health-check/health-check.module.ts
+++ src/modules/health-check/health-check.module.ts
@@ -5,14 +5,7 @@ import type { ModuleFactory } from '@src/lib/lucky-server';
 export class HealthCheckModule implements ModuleFactory {
   constructor(private readonly app: Application) {}
 
-  async init(): Promise<void> {
-    // Only attach routes if running as a standalone micro-service
-    if (process.env.IS_STANDALONE_MICRO_SERVICES) {
-      this.attachControllers();
-    }
-  }
-
-  private attachControllers(): void {
+  async init() {
     const controller = new HealthCheckController(this.app);
 
     controller.registerRoutes();
