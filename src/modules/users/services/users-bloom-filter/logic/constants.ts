c22d3d66cd6f3eda73b06dc2c6f2925592e77ea8
Tal Kohavy <tal.kohavy@controlup.com>
2026-06-25 22:08:01 +0300

    100k users isn't enough. increased to 5M

--- src/modules/users/services/users-bloom-filter/logic/constants.ts
+++ src/modules/users/services/users-bloom-filter/logic/constants.ts
@@ -1,3 +1,3 @@
 export const USERS_BLOOM_FILTER_KEY = 'users:bloom_filter';
 export const BLOOM_FILTER_ERROR_RATE = 0.01;
-export const BLOOM_FILTER_INITIAL_CAPACITY = 100_000;
+export const BLOOM_FILTER_INITIAL_CAPACITY = 5_000_000;
